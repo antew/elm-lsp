dc54a28837cecba5f060b02309608961173922cc
Antew <antewcode@gmail.com>
2019-03-20 21:50:05 -0400

    Tweak error message

--- lib/index.ts
+++ lib/index.ts
@@ -5,7 +5,7 @@ import * as fs from 'fs';
 
 const packageFileExists = fs.existsSync('./elm.json');
 if (!packageFileExists) {
-    console.log('There is no elm.json file in this directory. elm-analyse will only work in directories where such a file is located.');
+    console.log('There is no elm.json file in this directory. elm-lsp will only work in directories where such a file is located.');
     process.exit(1);
 }
 
